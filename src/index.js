8517c8bf2aeb1cc8d76544a8ddf236f91e18673f
Stepan <kurakste@gmail.com>
2019-06-26 22:35:35 +0300

    add vegetation on field

--- src/index.js
+++ src/index.js
@@ -12,9 +12,15 @@ getSprites(spritesPath)
     const app = new App(canva, 'lightblue', sprites);
     const tank = new Tank(10, 10, app);
     const veg = new Vegetation(230, 150, 'first', app);
+    const veg1 = new Vegetation(130, 250, 'first', app);
+    const veg2 = new Vegetation(30, 350, 'first', app);
+    const veg3 = new Vegetation(230, 50, 'first', app);
 
     app.addFigure(tank);
     app.addFigure(veg);
+    app.addFigure(veg1);
+    app.addFigure(veg2);
+    app.addFigure(veg3);
     app.init();
     setInterval(app.ticker, 50);
 
